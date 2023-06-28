d354775256727df19c3e0955be12c878e61acdc1
Horia <horiamanea.m@gmail.com>
2023-06-28 21:11:01 +0300

    prima lectie rect cu Leo

---
+++ src/Square.js
@@ -0,0 +1,9 @@
+export default function Square({value}) {
+
+    function handleClick() {
+        console.log('clicked', value)
+    }
+
+    return<button className="square"
+    onClick={handleClick}>{value}</button>
+}
