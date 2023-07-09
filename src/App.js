f40f202be5188bfefd8bbea8ff2859426abddf85
H0YA <horiamanea.m@gmail.com>
2023-07-09 20:39:31 +0300

    aici am invatat ce este un map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
-import Square from "./Square";
 
-export default function Board() {
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
   const emptyArray = Array(9).fill(null);
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(emptyArray);
@@ -42,9 +49,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -78,5 +83,27 @@ export default function Board() {
       {/* <hr></hr> */}
       {/* <Square value={"p"} onSquareClick={() => console.log("log")}></Square> */}
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handPlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
---
+++ src/ceestemap.js
@@ -0,0 +1,5 @@
+const xs = [5, 6, 1, 7, 9];
+console.log(xs);
+const add1 = (x) => x + 1;
+const rezultat = xs.map(add1);
+console.log(rezultat);
